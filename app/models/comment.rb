adf407b6b50d2f25c63d9d293644577836ec9d16
Roman Okhotnikov <mail@okht.ru>
2019-04-02 22:51:21 +0300

    Getting start 6

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,3 @@
-class CreateComments < ActiveRecord::Migration[5.0]
-  def change
-    create_table :comments do |t|
-      t.string :commenter
-      t.text :body
-      t.references :article, foreign_key: true
-
-      t.timestamps
-    end
-  end
+class Comment < ApplicationRecord
+  belongs_to :article
 end
