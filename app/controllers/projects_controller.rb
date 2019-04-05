dde42d1ae5600b1448034dc2be3205305aef782c
Roman Okhotnikov <mail@okht.ru>
2019-04-06 02:43:34 +0300

    8 part. 2 unit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,17 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = Project.find(params[id])
   end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,12 @@
 class TodosController < ApplicationController
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:text)
+  end
+
 end
